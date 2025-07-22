82eb5177b06929342a0bbbe3a674e86ce91d7703
M-Burak-Yilmazer <mbyilmazer38@gmail.com>
2025-07-22 15:31:48 +0300

    feat: Initialize controllers, models, routes, and middlewares for the Node.js Express application
    - Added controllers for brand, category, firm, product, purchase, sale, token, and user.
    - Implemented user controller with CRUD operations and Swagger documentation.
    - Created helper functions for password encryption and email sending.
    - Developed middleware for authentication, error handling, filtering, searching, sorting, pagination, logging, permissions, and file uploads.
    - Established models for brand, category, firm, product, purchase, sale, token, and user with Mongoose.
    - Set up routes for authentication, brand, category, firm, product, purchase, sale, token, and user.
    - Configured Swagger documentation generation for API endpoints.

---
+++ src/controllers/user.js
@@ -0,0 +1,92 @@
+"use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+
+//user Controller
+
+const User = require("../models/user");
+
+module.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
+
+    const data = await res.getModelList(User);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(User),
+      message: "success",
+      data,
+    });
+  },
+  create: async (req, res) => {
+     /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+
+const data= await User.create(req.body);
+    res.status(201).send({
+      error: false,
+      message: "success",
+      data,
+    });
+
+  },
+  read: async (req, res) => {
+/*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
+
+
+  },
+  update: async (req, res) => {
+       /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
+
+  },
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
+       
+  },
+};
